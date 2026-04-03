e09dc8f368504dd86b3fa98ee295a18f0bd4993c
Billy Nguyen <158776489+bnguye04@users.noreply.github.com>
2026-04-02 23:40:20 -0400

    Replace express-graphql with graphql-http to support graphql v16

--- logic/graphql_setup/graphql_implementation.js
+++ logic/graphql_setup/graphql_implementation.js
@@ -1,6 +1,6 @@
 // graphql_implementation.js
 import express from 'express';
-import { graphqlHTTP } from 'express-graphql';
+import { createHandler } from 'graphql-http/lib/use/express.js';
 import schema from './schema.js';
 import { root } from './resolvers.js';
 import { initDatabase } from '../database/sqlite.js';
@@ -12,11 +12,30 @@ export function startGraphQL() {
     initDatabase();
     console.log('✅ GraphQL database initialized');
 
-    app.use('/graphql', graphqlHTTP({
-      schema,
-      rootValue: root,
-      graphiql: true
-    }));
+    app.get('/graphql', (req, res) => {
+      res.type('html');
+      res.end(`<!DOCTYPE html>
+<html>
+  <head>
+    <title>GraphiQL</title>
+    <link rel="stylesheet" href="https://unpkg.com/graphiql/graphiql.min.css" />
+  </head>
+  <body style="margin: 0;">
+    <div id="graphiql" style="height: 100vh;"></div>
+    <script src="https://unpkg.com/react/umd/react.production.min.js"></script>
+    <script src="https://unpkg.com/react-dom/umd/react-dom.production.min.js"></script>
+    <script src="https://unpkg.com/graphiql/graphiql.min.js"></script>
+    <script>
+      ReactDOM.render(
+        React.createElement(GraphiQL, { fetcher: GraphiQL.createFetcher({ url: '/graphql' }) }),
+        document.getElementById('graphiql')
+      );
+    </script>
+  </body>
+</html>`);
+    });
+
+    app.use('/graphql', createHandler({ schema, rootValue: root }));
 
     app.listen(4000, () => console.log('GraphQL running on http://localhost:4000/graphql'));
   } catch (error) {
